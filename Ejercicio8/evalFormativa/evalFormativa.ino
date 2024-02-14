e4dc94f8d50b2f6541dbce786e662ba3e1e150a6
Alafresh <alafresh16@gmail.com>
2024-02-14 09:35:45 -0500

    Create evalFormativa.ino

---
+++ Ejercicio8/evalFormativa/evalFormativa.ino
@@ -0,0 +1,54 @@
+
+// Definición de estados
+enum class States {
+  ONE_SECOND,
+  TWO_SECONDS,
+  THREE_SECONDS
+};
+
+// Estado inicial
+States state = States::ONE_SECOND;
+
+// Variable para almacenar la última vez que se envió un mensaje
+unsigned long lastTime = 0;
+
+// Intervalos de tiempo (en milisegundos)
+const unsigned long oneSecond = 1000;
+const unsigned long twoSeconds = 2000;
+const unsigned long threeSeconds = 3000;
+
+void setup() {
+  // Iniciar comunicación serial
+  Serial.begin(9600);
+  Serial.println("1 segundo");
+}
+
+void loop() {
+  // Obtener el tiempo actual
+  unsigned long currentTime = millis();
+
+  // Máquina de estados
+  switch (state) {
+    case States::ONE_SECOND:
+      if (currentTime - lastTime >= oneSecond) {
+        Serial.println("1 segundo");
+        lastTime = currentTime;
+        state = States::TWO_SECONDS;
+      }
+      break;
+    case States::TWO_SECONDS:
+      if (currentTime - lastTime >= twoSeconds) {
+        Serial.println("2 segundos");
+        lastTime = currentTime;
+        state = States::THREE_SECONDS;
+      }
+      break;
+    case States::THREE_SECONDS:
+      if (currentTime - lastTime >= threeSeconds) {
+        Serial.println("3 segundos");
+        lastTime = currentTime;
+        state = States::ONE_SECOND;
+      }
+      break;
+  }
+}
